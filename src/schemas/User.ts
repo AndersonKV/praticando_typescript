779be323a67391df4de38cfa4d6b732321450ec5
AndersonKV <48493444+AndersonKV@users.noreply.github.com>
2020-01-04 14:20:48 -0300

    Add files via upload

---
+++ src/schemas/User.ts
@@ -0,0 +1,22 @@
+import { Schema, model, Document } from 'mongoose'
+
+interface userInterface extends Document {
+    email?: string,
+    firstName?: string,
+    lastName?: string,
+    fullName(): string
+}
+
+const UserSchema = new Schema({
+    email: String,
+    firstName: String,
+    lastName: String,
+}, {
+    timestamps: true
+})
+
+UserSchema.methods.fullName = function(): string {
+    return this.firstName + ' ' + this.lastName
+}
+
+export default model<userInterface>('TypeUser', UserSchema)
